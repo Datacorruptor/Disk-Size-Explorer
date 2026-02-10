2d6825d93cbde6dc76add7cb1171ebba385d9dab
adminq7dng37 <coder@coder.coder>
2026-02-10 12:39:59 +0300

    reworked symlink size calculation

--- scanner.cpp
+++ scanner.cpp
@@ -29,48 +29,39 @@ static QString joinPath(const QString& base,
 DirectoryScanner::DirectoryScanner(QObject* parent)
     : QObject(parent)
 {
-    // your init code
 }
 
-// --------------------
-// Count files (for progress)
-// --------------------
 
-uint64_t DirectoryScanner::countFiles(const QString& path, bool doPrint)
+qint64 getFileSize(const QString &path)
 {
-    if (doPrint)
-        qInfo() << "Started files count" << " " << path;
-    uint64_t count = 0;
+    qint64 size = 0;
+    QFileInfo fileInfo(path);
 
-    WIN32_FIND_DATAW data;
-    std::wstring search = toW(path) + L"\\*";
+    if(fileInfo.isSymLink() && fileInfo.size() == QFileInfo(fileInfo.symLinkTarget()).size())
+    {
+        // Try this approach first
+        QFile file(path);
+        if(file.exists() && file.open(QIODevice::ReadOnly))
+            size = file.size();
+        file.close();
 
-    HANDLE h = FindFirstFileW(search.c_str(), &data);
-    if (h == INVALID_HANDLE_VALUE)
-        return 0;
+        // If that didn't work, try this
+        if(size == 0)
+        {
+            QString tmpPath = path+".tmp";
+            for(int i=2; QFileInfo().exists(tmpPath); ++i) // Make sure filename is unique
+                tmpPath = path+".tmp"+QString::number(i);
 
-    do {
-        std::wstring wname = data.cFileName;
-        if (wname == L"." || wname == L"..")
-            continue;
-
-        QString name = QString::fromStdWString(wname);
-        QString childPath = joinPath(path, name);
-
-        if (data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-            count += countFiles(childPath, false);
-            if (doPrint)
-                qInfo() << count << " " <<"Files counted" << "+" << childPath;
-        } else {
-            count++;
-            if (doPrint)
-                qInfo() << count << " " <<"Files counted" << "+" << childPath;
+            if(QFile::copy(path, tmpPath))
+            {
+                size = QFileInfo(tmpPath).size();
+                QFile::remove(tmpPath);
+            }
         }
+    }
+    else size = fileInfo.size();
 
-    } while (FindNextFileW(h, &data));
-
-    FindClose(h);
-    return count;
+    return size;
 }
 
 // --------------------
@@ -107,7 +98,7 @@ QPair<uint64_t,uint64_t> DirectoryScanner::scan(std::shared_ptr<Node> node,
         child->name = info.fileName();
         child->cachedPath = joinPath(node->path(), info.fileName());
         child->parent = node;
-        child->isDir = info.isDir();
+        child->isDir = info.isDir() && !info.isSymLink();
         child->lastModified = info.lastModified().toSecsSinceEpoch();
 
         if (level == 0)
@@ -128,7 +119,7 @@ QPair<uint64_t,uint64_t> DirectoryScanner::scan(std::shared_ptr<Node> node,
             if (level == 0)
                 emit nodeFinished(0, child);
         } else {
-            child->size = info.size();
+            child->size = getFileSize(child->path());
             totalSize += child->size;
             totalChildren += 1;
 
@@ -145,83 +136,6 @@ QPair<uint64_t,uint64_t> DirectoryScanner::scan(std::shared_ptr<Node> node,
 
         node->children.push_back(std::move(child));
     }
-
-    /*
-    WIN32_FIND_DATAW data;
-    std::wstring search = toW(node->path()) + L"\\*";
-
-    HANDLE hFind = FindFirstFileW(search.c_str(), &data);
-    if (hFind == INVALID_HANDLE_VALUE)
-        return {0,0};
-
-    do {
-        if (cancel)
-            break;
-
-        std::wstring wname = data.cFileName;
-        if (wname == L"." || wname == L"..")
-            continue;
-
-        QString name = QString::fromStdWString(wname);
-
-        auto child = std::make_shared<Node>();
-        child->name = name;
-        child->cachedPath = joinPath(node->path(), name);
-        child->parent = node;
-        child->isDir = (data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY);
-
-        // Last modified time
-        FILETIME ft = data.ftLastWriteTime;
-        ULARGE_INTEGER ull;
-        ull.LowPart = ft.dwLowDateTime;
-        ull.HighPart = ft.dwHighDateTime;
-        child->lastModified =
-            static_cast<std::time_t>(
-                (ull.QuadPart - 116444736000000000ULL) / 10000000ULL
-                );
-
-        if (level == 0)
-            emit nodeStarted(0, child);
-
-        if (child->isDir) {
-            if (level == 0)
-                qInfo() << "scanning " << child->path();
-
-            if (level == 1){
-                l1parent = node;
-            }
-            auto result = scan(child, l1parent, cancel, level+1);
-            totalSize += result.first;
-            totalChildren += result.second;
-
-
-            if (level == 0)
-                emit nodeFinished(0, child);
-        } else {
-            LARGE_INTEGER s;
-            s.HighPart = data.nFileSizeHigh;
-            s.LowPart = data.nFileSizeLow;
-
-            child->size = s.QuadPart;
-            totalSize += child->size;
-            totalChildren += 1;
-
-            if (l1parent) {
-                emit nodeUpdated(child->size, l1parent);
-            }
-
-            //one file processed
-            emit nodeFinished(child->size, nullptr);
-
-            if (level == 0)
-                emit nodeFinished(0, child);
-        }
-
-        node->children.push_back(std::move(child));
-
-    } while (FindNextFileW(hFind, &data));
-
-    FindClose(hFind);*/
 
     // Sort largest first
     std::sort(node->children.begin(), node->children.end(),
